40f2f3b73472f52c9fd314be5e1696990cf331fd
Stephen Sinclair <radarsat1@gmail.com>
2009-12-29 03:21:19 +0000

    Move controller and admin tests into their own test programs.

---
+++ test/testadmin.c
@@ -0,0 +1,61 @@
+
+#include "../src/mapper_internal.h"
+#include <mapper/mapper.h>
+#include <stdio.h>
+#include <math.h>
+
+#include <unistd.h>
+#include <arpa/inet.h>
+
+mapper_admin my_admin = NULL;
+
+int test_admin()
+{
+    int error=0, wait;
+
+    my_admin = mapper_admin_new("tester", MAPPER_DEVICE_SYNTH, 8000);
+    if (!my_admin)
+    {
+        printf("Error creating admin structure.\n");
+        return 1;
+    }
+    printf("Admin structure initialized.\n");
+
+    printf("Found interface %s has IP %s\n", my_admin->interface,
+           inet_ntoa(my_admin->interface_ip));
+
+    while (    !my_admin->port.locked
+            || !my_admin->ordinal.locked )
+    {
+        usleep(10000);
+        mapper_admin_poll(my_admin);
+    }
+
+    printf("Allocated port %d.\n", my_admin->port.value);
+    printf("Allocated ordinal %d.\n", my_admin->ordinal.value);
+
+    printf("Delaying for 5 seconds..\n");
+    wait = 500;
+    while (wait-- >= 0)
+    {
+        usleep(10000);
+        mapper_admin_poll(my_admin);
+    }
+
+    mapper_admin_free(my_admin);
+    printf("Admin structure freed.\n");
+
+    return error;
+}
+
+int main()
+{
+    int result = test_admin();
+    if (result) {
+        printf("Test FAILED.\n");
+        return 1;
+    }
+
+    printf("Test PASSED.\n");
+    return 0;
+}
--- test/testsend.c
+++ test/testsend.c
@@ -7,47 +7,6 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
-mapper_admin my_admin = NULL;
-
-int test_admin()
-{
-    int error=0, wait;
-
-    my_admin = mapper_admin_new("tester", MAPPER_DEVICE_SYNTH, 8000);
-    if (!my_admin)
-    {
-        printf("Error creating admin structure.\n");
-        return 1;
-    }
-    printf("Admin structure initialized.\n");
-
-    printf("Found interface %s has IP %s\n", my_admin->interface,
-           inet_ntoa(my_admin->interface_ip));
-
-    while (    !my_admin->port.locked
-            || !my_admin->ordinal.locked )
-    {
-        usleep(10000);
-        mapper_admin_poll(my_admin);
-    }
-
-    printf("Allocated port %d.\n", my_admin->port.value);
-    printf("Allocated ordinal %d.\n", my_admin->ordinal.value);
-
-    printf("Delaying for 5 seconds..\n");
-    wait = 500;
-    while (wait-- >= 0)
-    {
-        usleep(10000);
-        mapper_admin_poll(my_admin);
-    }
-
-    mapper_admin_free(my_admin);
-    printf("Admin structure freed.\n");
-
-    return error;
-}
-
 int test_controller()
 {
     mapper_device md = mdev_new("tester", 9000);
@@ -93,13 +52,7 @@ int test_controller()
 
 int main()
 {
-    int result = test_admin();
-    if (result) {
-        printf("Test FAILED.\n");
-        return 1;
-    }
-
-    result = test_controller();
+    int result = test_controller();
     if (result) {
         printf("Test FAILED.\n");
         return 1;
