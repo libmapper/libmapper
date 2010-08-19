dfd646dca5599863ac9e7db70aa3ee990fddf7ab
Joseph Malloch <joseph.malloch@gmail.com>
2010-08-19 23:29:41 +0000

    max libmapper object compiles, instantiates, registers on network :)

--- maxbuild/maxadmin.c
+++ maxbuild/maxadmin.c
@@ -2,18 +2,18 @@
 	@file
 	maxadmin - max object interface with libmapper
     http://www.idmil.org/software/mappingtools
-	joseph malloch	
+	joseph malloch
 */
 
 #include "ext.h"							// standard Max include, always required
 #include "ext_obex.h"						// required for new style Max object
-#include "../src/operations.h"
-#include "../src/expression.h"
-#include "../src/mapper_internal.h"
-#include <mapper/mapper.h>
+#include "./src/operations.h"
+#include "./src/expression.h"
+#include "./src/mapper_internal.h"
+#include "./include/mapper/mapper.h"
 #include <stdio.h>
 #include <math.h>
-#include <lo/lo.h>
+#include "lo/lo.h"
 
 #include <unistd.h>
 #include <arpa/inet.h>
@@ -29,7 +29,8 @@ typedef struct _maxadmin
     void *m_outlet3;
     void *m_clock;          // pointer to clock object
     mapper_device device;
-    
+    mapper_signal sendsig;
+    int ready;
 } t_maxadmin;
 
 int recvport = 9000;
@@ -43,6 +44,7 @@ void maxadmin_assist(t_maxadmin *x, void *b, long m, long a, char *s);
 void maxadmin_anything(t_maxadmin *x, t_symbol *msg, long argc, t_atom *argv);
 void maxadmin_add_signal(t_maxadmin *x, t_symbol *msg, long argc, t_atom *argv);
 void maxadmin_remove_signal(t_maxadmin *x, t_symbol *msg, long argc, t_atom *argv);
+void poll(t_maxadmin *x);
 
 //////////////////////// global class pointer variable
 void *maxadmin_class;
@@ -80,17 +82,19 @@ void maxadmin_assist(t_maxadmin *x, void *b, long m, long a, char *s)
 
 void maxadmin_free(t_maxadmin *x)
 {
-	//freeobject((t_object *) x->m_proxy);
-    if (device) {
-        if (device->routers) {
+    clock_unset(x->m_clock);	// Remove clock routine from the scheduler
+	clock_free(x->m_clock);		// Frees memeory used by clock
+    
+    if (x->device) {
+        if (x->device->routers) {
             post("Removing router.. ");
-            fflush(stdout);
-            mdev_remove_router(device, device->routers);
+            //fflush(stdout);
+            mdev_remove_router(x->device, x->device->routers);
             post("ok\n");
         }
         post("Freeing device.. ");
-        fflush(stdout);
-        mdev_free(device);
+        //fflush(stdout);
+        mdev_free(x->device);
         post("ok\n");
     }
 }
@@ -111,23 +115,24 @@ void maxadmin_anything(t_maxadmin *x, t_symbol *msg, long argc, t_atom *argv)
 }
 
 /*! Creation of a local sender. */
-int setup_device()
+int setup_device(t_maxadmin *x)
 {
     //use dummy name for now
-    sender = mdev_new("testsend", sendport);
-    if (!sender)
-        post("ERROR: Sender NOT created.\n");
+    x->device = mdev_new("maxadmin", sendport);
+    if (!x->device)
+        return 1;
     else
-        post("Sender created.\n");
+        post("Device created.\n");
     
     // create a dummy signal for now
-    sendsig = msig_float(1, "/outsig", 0, 0, 1, 0, 0, 0);
+    x->sendsig = msig_float(1, "/outsig", 0, 0, 1, 0, 0, 0);
     
-    mdev_register_output(sender, sendsig);
+    mdev_register_output(x->device, x->sendsig);
     
     post("Output signal /outsig registered.\n");
-    post("Number of outputs: %d\n", mdev_num_outputs(sender));
-
+    post("Number of outputs: %d\n", mdev_num_outputs(x->device));
+    
+    return 0;
 }
 
 
@@ -157,19 +162,29 @@ void *maxadmin_new(t_symbol *s, long argc, t_atom *argv)
         }
     }
     
-    if (setup_device()) {
-        printf("Error initializing device.\n");
-        x->m_clock = clock_new(x, (method)poll);	// Create the timing clock
+    if (setup_device(x)) {
+        post("Error initializing device.\n");
     }
     
+    x->ready = 0;
     
+    x->m_clock = clock_new(x, (method)poll);	// Create the timing clock
+    
+    clock_delay(x->m_clock, INTERVAL);  // Set clock to go off after delay
     
 	return (x);
 }
 
 void poll(t_maxadmin *x)
 {
-	poll(x->m_clock, INTERVAL);  // Set clock to go off after delay
-	
-	mdev_poll(device, 0);
+	clock_delay(x->m_clock, INTERVAL);  // Set clock to go off after delay
+    
+    mdev_poll(x->device, 0);
+    
+    if (!x->ready) {
+        if (mdev_ready(x->device)) {
+            mapper_db_dump();
+            x->ready = 1;
+        }
+    }
 }
