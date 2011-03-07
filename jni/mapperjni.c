036a5b6d5f576a520838bf3c91267c21e2301998
Stephen Sinclair <radarsat1@gmail.com>
2011-03-07 13:22:25 -0500

    JNI bindings: call input signal listener.
    Works, but needs improvement: JVM environment is passed through a
    global (not thread-safe), and a global reference to the listener is
    created which is never deleted.

--- jni/mapperjni.c
+++ jni/mapperjni.c
@@ -3,6 +3,8 @@
 
 #include "Mapper_Device.h"
 
+JNIEnv *genv=0;
+
 JNIEXPORT void JNICALL Java_Mdev_t
   (JNIEnv *env, jobject obj, jint i, jstring s)
 {
@@ -50,12 +52,33 @@ JNIEXPORT void JNICALL Java_Mapper_Device_mdev_1free
 JNIEXPORT int JNICALL Java_Mapper_Device_mdev_1poll
   (JNIEnv *env, jobject obj, jlong d, jint timeout)
 {
+    genv = env;
     mapper_device dev = (mapper_device)d;
     return mdev_poll(dev, timeout);
 }
 
+static void java_msig_input_cb(mapper_signal sig, void *v)
+{
+    printf("in callback, genv=%p\n", genv);
+    mapper_db_signal props = msig_properties(sig);
+    jobject listener = (jobject)props->user_data;
+    if (listener) {
+        jclass cls = (*genv)->GetObjectClass(genv, listener);
+        printf("listener class: %p\n", cls);
+        if (cls) {
+            jmethodID val = (*genv)->GetMethodID(genv, cls, "onInput", "()V");
+            if (val) {
+                printf("got onInput: %p\n", val);
+                (*genv)->CallVoidMethod(genv, listener, val);
+            }
+            else
+                printf("couldn't get onInput\n");
+        }
+    }
+}
+
 JNIEXPORT jlong JNICALL Java_Mapper_Device_mdev_1add_1input
-  (JNIEnv *env, jobject obj, jlong d, jstring name, jint length, jchar type, jstring unit, jobject minimum, jobject maximum, jobject handler)
+  (JNIEnv *env, jobject obj, jlong d, jstring name, jint length, jchar type, jstring unit, jobject minimum, jobject maximum, jobject listener)
 {
     if (!d || !name || (length<=0) || (type!='f' && type!='i'))
         return 0;
@@ -100,7 +123,8 @@ JNIEXPORT jlong JNICALL Java_Mapper_Device_mdev_1add_1input
     mapper_signal s = mdev_add_input(dev, cname, length, type, cunit,
                                      minimum ? &mn : 0,
                                      maximum ? &mx : 0,
-                                     0, 0);
+                                     java_msig_input_cb,
+                                     (*env)->NewGlobalRef(env, listener));
 
     (*env)->ReleaseStringUTFChars(env, name, cname);
     if (unit) (*env)->ReleaseStringUTFChars(env, unit, cunit);
