889087581366242b7476d9f36368ade5c81a8a38
Joseph Malloch <joseph.malloch@gmail.com>
2012-03-06 19:08:07 -0500

    Added rough version of testinstance.py

---
+++ swig/testinstance.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+import sys, mapper, random
+
+def h(sig, f):
+    try:
+        print 'received response:', f
+    except:
+        print 'exception'
+        print sig, f
+
+def hi(sig, f, id):
+    try:
+        print 'received instance:', id, f
+    except:
+        print 'exception'
+        print sig, id, f
+
+src = mapper.device("src", 9000)
+outsig = src.add_output("/outsig", 1, 'f', None, 0, 1000)
+outsig.reserve_instances(5)
+
+dest = mapper.device("dest", 9000)
+insig = dest.add_input("/insig", 1, 'f', None, 0, 1, h)
+insig.reserve_instances(5, hi)
+
+while not src.ready() or not dest.ready():
+    src.poll()
+    dest.poll(10)
+
+monitor = mapper.monitor()
+
+monitor.link('%s' %src.name, '%s' %dest.name)
+monitor.connect('%s%s' %(src.name, outsig.name),
+                '%s%s' %(dest.name, insig.name),
+                {'mode': mapper.MO_LINEAR})
+
+for i in range(1000):
+    r = random.randint(0,5)
+    id = random.randint(0,10)
+    if r == 0:
+        print 'retiring sender instance', id
+        insig.release_instance(id)
+    else:    
+        print 'updating instance', id, 'to', i
+        insig.update_instance(id, i)
+    src.poll(10)
+    dest.poll(10)
