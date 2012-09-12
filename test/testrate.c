1b141eeb5bb7e8cde7f3a5b03f1a5d6b7e9aac59
Stephen Sinclair <radarsat1@gmail.com>
2012-09-11 20:19:27 -0400

    Add a test for rate feature.

---
+++ test/testrate.c
@@ -0,0 +1,199 @@
+
+#include "../src/mapper_internal.h"
+#include <mapper/mapper.h>
+#include <stdio.h>
+#include <math.h>
+#include <lo/lo.h>
+
+#include <unistd.h>
+#include <signal.h>
+
+#ifdef WIN32
+#define usleep(x) Sleep(x/1000)
+#endif
+
+int automate = 1;
+
+mapper_device source = 0;
+mapper_device destination = 0;
+mapper_signal sendsig = 0;
+mapper_signal recvsig = 0;
+
+int port = 9000;
+
+int sent = 0;
+int received = 0;
+int done = 0;
+
+/*! Creation of a local source. */
+int setup_source()
+{
+    source = mdev_new("testsend", port, 0);
+    if (!source)
+        goto error;
+    printf("source created.\n");
+
+    float mn=0, mx=10;
+
+    sendsig = mdev_add_output(source, "/outsig", 1, 'f', "Hz", &mn, &mx);
+
+    // This signal will be updated at 100 Hz
+    msig_set_rate(sendsig, 100);
+
+    printf("Output signal /outsig registered.\n");
+
+    return 0;
+
+  error:
+    return 1;
+}
+
+void cleanup_source()
+{
+    if (source) {
+        if (source->routers) {
+            printf("Removing router.. ");
+            fflush(stdout);
+            mdev_remove_router(source, source->routers);
+            printf("ok\n");
+        }
+        printf("Freeing source.. ");
+        fflush(stdout);
+        mdev_free(source);
+        printf("ok\n");
+    }
+}
+
+void insig_handler(mapper_signal sig, mapper_db_signal props,
+                   mapper_timetag_t *timetag, void *value)
+{
+    if (value) {
+        printf("--> destination got %s", props->name);
+        float *v = value;
+        for (int i = 0; i < props->length; i++) {
+            printf(" %f", v[i]);
+        }
+        printf("\n");
+    }
+    received++;
+}
+
+/*! Creation of a local destination. */
+int setup_destination()
+{
+    destination = mdev_new("testrecv", port, 0);
+    if (!destination)
+        goto error;
+    printf("destination created.\n");
+
+    float mn=0, mx=1;
+
+    recvsig = mdev_add_input(destination, "/insig", 1, 'f',
+                               0, &mn, &mx, insig_handler, 0);
+
+    // This signal is expected to be updated at 100 Hz
+    msig_set_rate(recvsig, 100);
+
+    printf("Input signal /insig registered.\n");
+
+    return 0;
+
+  error:
+    return 1;
+}
+
+void cleanup_destination()
+{
+    if (destination) {
+        printf("Freeing destination.. ");
+        fflush(stdout);
+        mdev_free(destination);
+        printf("ok\n");
+    }
+}
+
+
+
+void wait_local_devices()
+{
+    while (!(mdev_ready(source) && mdev_ready(destination))) {
+        mdev_poll(source, 0);
+        mdev_poll(destination, 0);
+
+        usleep(50 * 1000);
+    }
+}
+
+void loop()
+{
+    int i = 0;
+
+    if (automate) {
+        char source_name[1024], destination_name[1024];
+
+        printf("%s\n", mdev_name(source));
+        printf("%s\n", mdev_name(destination));
+
+        lo_address a = lo_address_new_from_url("osc.udp://224.0.1.3:7570");
+        lo_address_set_ttl(a, 1);
+
+        lo_send(a, "/link", "ss", mdev_name(source), mdev_name(destination));
+
+        msig_full_name(sendsig, source_name, 1024);
+        msig_full_name(recvsig, destination_name, 1024);
+
+        lo_send(a, "/connect", "ss", source_name, destination_name);
+
+        lo_address_free(a);
+    }
+
+    float phasor[10];
+    for (i=0; i<10; i++)
+        phasor[i] = i;
+
+    while (i >= 0 && !done) {
+        mdev_poll(source, 0);
+
+        // 10 times a second, we provide 10 samples, making a
+        // periodically-sampled signal of 100 Hz.
+        printf("Sending [%g..%g]...\n", phasor[0], phasor[9]);
+
+        msig_update(sendsig, phasor, 10);
+        int r = mdev_poll(destination, 100);
+        printf("Destination got %d message%s.\n", r, r==1?"":"s");
+        i++;
+    }
+}
+
+void ctrlc(int sig)
+{
+    done = 1;
+}
+
+int main()
+{
+    int result = 0;
+
+    signal(SIGINT, ctrlc);
+
+    if (setup_destination()) {
+        printf("Error initializing destination.\n");
+        result = 1;
+        goto done;
+    }
+
+    if (setup_source()) {
+        printf("Done initializing source.\n");
+        result = 1;
+        goto done;
+    }
+
+    wait_local_devices();
+
+    loop();
+
+  done:
+    cleanup_destination();
+    cleanup_source();
+    return result;
+}
