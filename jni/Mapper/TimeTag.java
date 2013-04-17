d820f071d6943922dee5f57d1541b941827efbcc
Stephen Sinclair <radarsat1@gmail.com>
2013-04-17 15:23:30 +0200

    JNI: Add an initializer for TimeTag supporting sec/frac pair.

--- jni/Mapper/TimeTag.java
+++ jni/Mapper/TimeTag.java
@@ -6,6 +6,14 @@ public class TimeTag
     public long sec;
     public long frac;
 
+    public static final TimeTag IMMEDIATE = new TimeTag(0, 1);
+
+    public TimeTag(long _sec, long _frac)
+    {
+        sec = _sec;
+        frac = _frac;
+    }
+
     public TimeTag(Double secondsSinceEpoch)
     {
         // TO DO
