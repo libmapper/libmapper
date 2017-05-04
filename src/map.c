libmapper/libmapper

commit 2478203fb9aa1aa5e34753cb209442d8ee4d92cb

Do not permit mapping signal to itself.

src/map.c
@@ -94,11 +94,21 @@ void mapper_map_init(mapper_map map)
 mapper_map mapper_map_new(int num_sources, mapper_signal *sources,
                           int num_destinations, mapper_signal *destinations)
 {
-    int i;
+    int i, j;
     if (!sources || !*sources || !destinations || !*destinations)
         return 0;
     if (num_sources <= 0 || num_sources > MAX_NUM_MAP_SOURCES)
         return 0;
+
+    for (i = 0; i < num_sources; i++) {
+        for (j = 0; j < num_destinations; j++) {
+            if (sources[i]->id == destinations[j]->id) {
+                trace("Cannot connect signal '%s:%s' to itself.\n",
+                      mdev_name(sources[i]->device), sources[i]->name);
+                return 0;
+            }
+        }
+    }
 
     // Only 1 destination supported for now
     if (num_destinations != 1)

src/network.c
@@ -1558,11 +1558,16 @@ static int parse_signal_names(const char *types, lo_arg **argv, int argc,
             *prop_index = *dest_index+1;
     }
 
-    // check that all signal names are well formed
+    /* Check that all signal names are well formed, and that no signal names
+     * appear in both source and destination lists. */
     for (i = 0; i < num_sources; i++) {
         if (!strchr((&argv[*src_index+i]->s)+1, '/')) {
             trace("malformed source signal name '%s'.\n", &argv[*src_index+i]->s);
             return 0;
+        }
+        if (strcmp(&argv[*src_index+i]->s, &argv[*dest_index]->s)==0) {
+            trace("prevented attempt to connect signal '%s' to itself.\n",
+                  argv[*dest_index]->s);
         }
     }
     if (!strchr((&argv[*dest_index]->s)+1, '/')) {

commit b885bfb4bd0808a631eb89c6b335e00f83178c23

Fixed 2 debug-mode-only bugs introduced in commit 2478203fb.

src/map.c
@@ -104,7 +104,7 @@ mapper_map mapper_map_new(int num_sources, mapper_signal *sources,
         for (j = 0; j < num_destinations; j++) {
             if (sources[i]->id == destinations[j]->id) {
                 trace("Cannot connect signal '%s:%s' to itself.\n",
-                      mdev_name(sources[i]->device), sources[i]->name);
+                      mapper_device_name(sources[i]->device), sources[i]->name);
                 return 0;
             }
         }

src/network.c
@@ -1567,7 +1567,7 @@ static int parse_signal_names(const char *types, lo_arg **argv, int argc,
         }
         if (strcmp(&argv[*src_index+i]->s, &argv[*dest_index]->s)==0) {
             trace("prevented attempt to connect signal '%s' to itself.\n",
-                  argv[*dest_index]->s);
+                  &argv[*dest_index]->s);
         }
     }
     if (!strchr((&argv[*dest_index]->s)+1, '/')) {