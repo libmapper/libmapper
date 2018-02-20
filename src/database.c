5bf9c752b17569e76310a910ffcfa9c7ed9dedf8
Joseph Malloch <joseph.malloch@gmail.com>
2018-02-20 14:02:46 -0400

    Ensure devices are marked as subscribed if manual subscription is added during temporary subscription window.

--- src/database.c
+++ src/database.c
@@ -1584,11 +1584,12 @@ void mapper_database_subscribe(mapper_database db, mapper_device dev, int flags,
             s->device->version = -1;
             s->next = db->subscriptions;
             db->subscriptions = s;
-            dev->subscribed = 1;
         }
-        else if (s->flags != flags)
-            s->device->version = -1;
+        dev->subscribed = 1;
+        if (s->flags == flags)
+            return;
 
+        s->device->version = -1;
         s->flags = flags;
 
         mapper_timetag_t tt;
