6491cecd8db16fba369cbbffa354396426fcb512
Joseph Malloch <joseph.malloch@gmail.com>
2019-12-27 15:52:57 -0400

    Add flags for auto-terminate and verbose mode to testsignals.

--- test/testsignals.c
+++ test/testsignals.c
@@ -11,7 +11,15 @@
 #define num_inputs 100
 #define num_outputs 100
 
+#define eprintf(format, ...) do {               \
+    if (verbose)                                \
+        fprintf(stdout, format, ##__VA_ARGS__); \
+} while(0)
+
 int wait_ms = 10000;
+int done = 0;
+int verbose = 1;
+int terminate = 0;
 
 mapper_device dev = 0;
 mapper_signal inputs[num_inputs];
@@ -21,21 +29,72 @@ void sig_handler(mapper_signal sig, mapper_id instance, const void *value,
                  int count, mapper_timetag_t *timetag)
 {
     if (value) {
-        printf("--> destination got %s", sig->name);
+        eprintf("--> destination got %s", sig->name);
         float *v = (float*)value;
         for (int i = 0; i < sig->length; i++) {
-            printf(" %f", v[i]);
+            eprintf(" %f", v[i]);
         }
-        printf("\n");
+        eprintf("\n");
     }
+}
+
+void loop()
+{
+    if (terminate) {
+        while (wait_ms > 0 && !done) {
+            eprintf("\rWaiting for %d ms.", wait_ms);
+            fflush(stdout);
+            mapper_device_poll(dev, 100);
+            wait_ms -= 100;
+        }
+    }
+    else {
+        while (!done) {
+            mapper_device_poll(dev, 100);
+        }
+    }
+}
+
+void ctrlc(int sig)
+{
+    done = 1;
 }
 
 int main(int argc, char ** argv)
 {
-    int i, result = 0;
+    int i, j, result = 0;
+
+    // process flags for -v verbose, -t terminate, -h help
+    for (i = 1; i < argc; i++) {
+        if (argv[i] && argv[i][0] == '-') {
+            int len = strlen(argv[i]);
+            for (j = 1; j < len; j++) {
+                switch (argv[i][j]) {
+                    case 'h':
+                        printf("test.c: possible arguments "
+                               "-q quiet (suppress output), "
+                               "-t terminate automatically, "
+                               "-h help\n");
+                        return 1;
+                        break;
+                    case 'q':
+                        verbose = 0;
+                        break;
+                    case 't':
+                        terminate = 1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    signal(SIGINT, ctrlc);
+
     char signame[32];
 
-    printf("Creating device... ");
+    eprintf("Creating device... ");
     fflush(stdout);
     dev = mapper_device_new("testsignals", 0, 0);
     if (!dev) {
@@ -46,10 +105,10 @@ int main(int argc, char ** argv)
         mapper_device_poll(dev, 100);
     }
 
-    printf("Adding %d signals...\n", num_inputs + num_outputs);
+    eprintf("Adding %d signals...\n", num_inputs + num_outputs);
     fflush(stdout);
     int max = num_inputs > num_outputs ? num_inputs : num_outputs;
-    for (i = 0; i < max; i++) {
+    for (i = 0; i < max && !done; i++) {
         mapper_device_poll(dev, 100);
         if (i < num_inputs) {
             snprintf(signame, 32, "in%i", i);
@@ -69,15 +128,10 @@ int main(int argc, char ** argv)
         }
     }
 
-    while (wait_ms > 0) {
-        printf("\rWaiting for %d ms.", wait_ms);
-        fflush(stdout);
-        mapper_device_poll(dev, 100);
-        wait_ms -= 100;
-    }
+    loop();
 
-    printf("\rRemoving %d signals...\n", num_inputs + num_outputs);
-    for (i = 0; i < max; i++) {
+    eprintf("\rRemoving %d signals...\n", num_inputs + num_outputs);
+    for (i = 0; i < max && !done; i++) {
         if (i < num_inputs)
             mapper_device_remove_signal(dev, inputs[i]);
         if (i < num_outputs)
@@ -88,6 +142,6 @@ int main(int argc, char ** argv)
   done:
     if (dev)
         mapper_device_free(dev);
-    printf("Test %s.\n", result ? "FAILED" : "PASSED");
+    eprintf("Test %s.\n", result ? "FAILED" : "PASSED");
     return result;
 }
