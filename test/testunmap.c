cc253cd6e0314ab8c73e44a1c9265ed4a732750e
Joseph Malloch <joseph.malloch@gmail.com>
2020-02-01 10:56:59 -0400

    Added test for link cleanup.

---
+++ test/testunmap.c
@@ -0,0 +1,234 @@
+
+#include "../src/mpr_internal.h"
+#include <mpr/mpr.h>
+#include <stdio.h>
+#include <math.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+
+#define eprintf(format, ...) do {               \
+    if (verbose)                                \
+        fprintf(stdout, format, ##__VA_ARGS__); \
+} while(0)
+
+int verbose = 1;
+int terminate = 0;
+int autoconnect = 1;
+int done = 0;
+
+mpr_dev src = 0;
+mpr_dev dst = 0;
+mpr_graph srcgraph = 0;
+mpr_graph dstgraph = 0;
+mpr_sig sendsig = 0;
+mpr_sig recvsig = 0;
+
+int src_linked = 0;
+int dst_linked = 0;
+
+int sent = 0;
+int received = 0;
+
+int setup_src(char *iface)
+{
+    src = mpr_dev_new("testunmap-send", 0);
+    if (!src)
+        goto error;
+    srcgraph = mpr_obj_get_graph((mpr_obj)src);
+    if (iface)
+        mpr_graph_set_interface(srcgraph, iface);
+    eprintf("source created.\n");
+
+    int mn=0, mx=1;
+    sendsig = mpr_sig_new(src, MPR_DIR_OUT, "outsig", 1, MPR_INT32, NULL,
+                          &mn, &mx, NULL, NULL, 0);
+
+    eprintf("Output signal 'outsig' registered.\n");
+    mpr_list l = mpr_dev_get_sigs(src, MPR_DIR_OUT);
+    eprintf("Number of outputs: %d\n", mpr_list_get_size(l));
+    mpr_list_free(l);
+
+    return 0;
+
+  error:
+    return 1;
+}
+
+void cleanup_src()
+{
+    if (src) {
+        eprintf("Freeing source.. ");
+        fflush(stdout);
+        mpr_dev_free(src);
+        eprintf("ok\n");
+    }
+}
+
+void handler(mpr_sig sig, mpr_sig_evt evt, mpr_id instance, int len,
+             mpr_type type, const void *val, mpr_time t)
+{
+    if (val) {
+        eprintf("handler: Got %f\n", (*(float*)val));
+    }
+    received++;
+}
+
+int setup_dst(char *iface)
+{
+    dst = mpr_dev_new("testunmap-recv", 0);
+    if (!dst)
+        goto error;
+    dstgraph = mpr_obj_get_graph((mpr_obj)dst);
+    if (iface)
+        mpr_graph_set_interface(dstgraph, iface);
+    eprintf("destination created.\n");
+
+    float mn=0, mx=1;
+    recvsig = mpr_sig_new(dst, MPR_DIR_IN, "insig", 1, MPR_FLT, NULL,
+                          &mn, &mx, NULL, handler, MPR_SIG_UPDATE);
+
+    eprintf("Input signal 'insig' registered.\n");
+    mpr_list l = mpr_dev_get_sigs(dst, MPR_DIR_IN);
+    eprintf("Number of inputs: %d\n", mpr_list_get_size(l));
+    mpr_list_free(l);
+
+    return 0;
+
+  error:
+    return 1;
+}
+
+void cleanup_dst()
+{
+    if (dst) {
+        eprintf("Freeing destination.. ");
+        fflush(stdout);
+        mpr_dev_free(dst);
+        eprintf("ok\n");
+    }
+}
+
+int setup_maps()
+{
+    mpr_map map = mpr_map_new(1, &sendsig, 1, &recvsig);
+
+    mpr_obj_push((mpr_obj)map);
+
+    // Wait until mapping has been established
+    while (!done && !mpr_map_get_is_ready(map)) {
+        mpr_dev_poll(src, 10);
+        mpr_dev_poll(dst, 10);
+    }
+
+    // release the map
+    mpr_map_release(map);
+
+    return 0;
+}
+
+void wait_ready()
+{
+    while (!done && !(mpr_dev_get_is_ready(src) && mpr_dev_get_is_ready(dst))) {
+        mpr_dev_poll(src, 25);
+        mpr_dev_poll(dst, 25);
+    }
+}
+
+void loop()
+{
+    eprintf("Polling device..\n");
+    int i = 0;
+    while ((!terminate || srcgraph->links || dstgraph->links) && !done) {
+        mpr_dev_poll(src, 0);
+        eprintf("Updating signal %s to %d\n", sendsig->name, i);
+        mpr_sig_set_value(sendsig, 0, 1, MPR_INT32, &i, MPR_NOW);
+        sent++;
+        mpr_dev_poll(dst, 100);
+        i++;
+
+        if (!verbose) {
+            printf("\r  Sent: %4i, Received: %4i   ", sent, received);
+            fflush(stdout);
+        }
+    }
+}
+
+void ctrlc(int signal)
+{
+    done = 1;
+}
+
+int main(int argc, char **argv)
+{
+    int i, j, result = 0;
+    char *iface = 0;
+
+    // process flags for -v verbose, -t terminate, -h help
+    for (i = 1; i < argc; i++) {
+        if (argv[i] && argv[i][0] == '-') {
+            int len = strlen(argv[i]);
+            for (j = 1; j < len; j++) {
+                switch (argv[i][j]) {
+                    case 'h':
+                        printf("testunmap.c: possible arguments "
+                               "-q quiet (suppress output), "
+                               "-t terminate automatically, "
+                               "-h help\n");
+                        return 1;
+                        break;
+                    case 'q':
+                        verbose = 0;
+                        break;
+                    case 't':
+                        terminate = 1;
+                        break;
+                    case '-':
+                        if (strcmp(argv[i], "--iface")==0 && argc>i+1) {
+                            i++;
+                            iface = argv[i];
+                            j = 1;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    signal(SIGINT, ctrlc);
+
+    if (setup_dst(iface)) {
+        eprintf("Error initializing destination.\n");
+        result = 1;
+        goto done;
+    }
+
+    if (setup_src(iface)) {
+        eprintf("Done initializing source.\n");
+        result = 1;
+        goto done;
+    }
+
+    wait_ready();
+
+    if (autoconnect && setup_maps()) {
+        eprintf("Error initializing maps.\n");
+        result = 1;
+        goto done;
+    }
+
+    loop();
+
+    if (srcgraph->links || dstgraph->links) {
+        eprintf("Link cleanup failed.\n");
+        result = 1;
+    }
+
+  done:
+    cleanup_dst();
+    cleanup_src();
+    printf("Test %s.\n", result ? "FAILED" : "PASSED");
+    return result;
+}
