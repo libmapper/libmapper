7eb954884e0cb6950e506acaf6a4793718272c3b
Brady Boettcher <bboettcher2@wisc.edu>
2022-02-23 18:12:14 -0500

    windows batch file for automatic building
    hopeful fix for python wheel building
    
    yaml spacing fix
    
    forgot to add setup file
    
    install wheel lib with pip
    
    one last fix for wheels

---
+++ windows_build.bat
@@ -0,0 +1,33 @@
+@echo off
+:: Create build directory
+if not exist build\ mkdir build
+cd build\
+:: Download zlib and extract zip
+if not exist zlib\ (
+  echo Downloading and extracting zlib into build/ folder...
+  powershell -Command "Invoke-WebRequest https://www.bruot.org/hp/media/files/libraries/zlib_1_2_11_msvc2017_64.zip -OutFile zlib.zip"
+  powershell -Command "Expand-Archive zlib.zip zlib"
+  del zlib.zip
+)
+:: Download liblo and build
+if not exist liblo\ (
+  echo Downloading and building liblo in build/ folder...
+  powershell -Command "Invoke-WebRequest https://github.com/bboettcher3/liblo/archive/refs/heads/master.zip -OutFile liblo.zip"
+  powershell -Command "Expand-Archive liblo.zip liblo"
+  del liblo.zip
+  cd liblo\liblo-master\cmake\
+  mkdir build
+  cd build\
+  powershell -Command "cmake .."
+  powershell -Command "cmake --build . --target all_build
+)
+:: Build libmapper
+echo "Building libmapper..."
+:: Reset active directory
+cd /D "%~dp0"
+cd build\
+powershell -Command "cmake .."
+powershell -Command "cmake --build . --target all_build
+echo Done! dll's for liblo and zlib are located in the build/ folder
+echo build/Debug/ contains the libmapper dll
+pause
