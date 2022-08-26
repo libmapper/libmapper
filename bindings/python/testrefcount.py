89393926b6934fe93cd52cf4d6173e4997be8278
Joseph Malloch <jmalloch@dal.ca>
2022-08-26 18:33:13 -0300

    Python bindings: added testrefcount.py

---
+++ bindings/python/testrefcount.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+import sys, random, libmapper as mpr
+
+def h(sig, event, id, val, time):
+    print('  handler got', sig['name'], '=', val, 'at time', time.get_double())
+
+src = mpr.Device('py.testrefcount.src')
+outsig = src.add_signal(mpr.Direction.OUTGOING, 'outsig', 10, mpr.Type.INT32, None, 0, 1)
+
+dest = mpr.Device('py.testrefcount.dst')
+
+num_dst = 5
+
+for i in range(num_dst):
+    # don't store references to input signals
+    dest.add_signal(mpr.Direction.INCOMING, 'insig%i' %i, 10, mpr.Type.FLOAT, None, 0, 1, None, h)
+
+while not src.ready or not dest.ready:
+    src.poll(10)
+    dest.poll(10)
+
+for s in dest.signals():
+    map = mpr.Map(outsig, s).push()
+
+while not map.ready:
+    src.poll(10)
+    dest.poll(10)
+
+for i in range(1000):
+    outsig.set_value([i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9])
+    dest.poll(10)
+    src.poll(10)
+
+src.free()
+dest.free()
