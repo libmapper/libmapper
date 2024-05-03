749fa2586a25dd9a805f1a1778531cd602c8e543
Kyle Smith <kyleafmine@gmail.com>
2024-05-03 10:34:36 -0300

    CSharp: Fixed timestamp retrieval

--- bindings/csharp/Mapper.cs
+++ bindings/csharp/Mapper.cs
@@ -50,7 +50,7 @@ namespace Mapper
         // internal long _time;
 
         [StructLayout(LayoutKind.Explicit)]
-        internal struct timeStruct
+        public struct MprTimeStruct
         {
             [FieldOffset(0)]
             internal long ntp;
@@ -59,7 +59,7 @@ namespace Mapper
             [FieldOffset(4)]
             internal UInt32 frac;
         }
-        internal timeStruct data;
+        internal MprTimeStruct data;
 
         public Time(long ntp)
             { data.ntp = ntp; }
@@ -1030,18 +1030,14 @@ namespace Mapper
         }
 
         [DllImport("mapper", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
-        unsafe private static extern void* mpr_sig_get_value(IntPtr sig, UInt64 id, ref long time);
+        unsafe private static extern void* mpr_sig_get_value(IntPtr sig, UInt64 id, ref Time.MprTimeStruct time);
         unsafe public (dynamic, Time) GetValue(UInt64 instanceId = 0)
         {
             int len = mpr_obj_get_prop_as_int32(this._obj, (int)Property.Length, null);
             int type = mpr_obj_get_prop_as_int32(this._obj, (int)Property.Type, null);
-            long time = 0;
+            var time = new Time.MprTimeStruct();
             void *val = mpr_sig_get_value(this._obj, instanceId, ref time);
-            if (time == 0) { // signal not written to yet, so time is a null pointer
-                return (BuildValue(len, type, val, 0), new Time());
-            } else {
-                return (BuildValue(len, type, val, 0), new Time(*(long*)time));
-            }
+            return (BuildValue(len, type, val, 0), new Time(time.ntp));
         }
         // unsafe public dynamic GetValue(UInt64 instanceId = 0)
         // {
