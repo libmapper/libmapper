4ca38d29efbcfb1b4c84ac15850322ca5c1f9a0f
Kyle Smith <kyleafmine@gmail.com>
2024-05-16 15:21:07 -0300

    CSharp: Add demo project

---
+++ bindings/csharp/Demo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Mapper.NET;
+using Type = Mapper.NET.Type;
+
+namespace Demo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var dev = new Device("CSharpDemo");
+        while (true)
+        {
+            if (dev.GetIsReady() == 1) break;
+            dev.Poll(100);
+        }
+        
+        var sigA = dev.AddSignal(Signal.Direction.Outgoing, "Sine", 1, Type.Double);
+        var sigB = dev.AddSignal(Signal.Direction.Incoming, "Debug_Log", 1, Type.Double);
+        
+        var map = new Map(sigA, sigB);
+        map.Push();
+        while (map.GetIsReady() == 0)
+        {
+            dev.Poll(100);
+        }
+        
+        Console.WriteLine("Map created SigA -> SigB");
+
+        while (true)
+        {
+            var value = Math.Sin(DateTime.Now.Second / 10.0);
+            sigA.SetValue(value);
+            Console.WriteLine($"Sent: {value}");
+            dev.Poll(500);
+            var received = sigB.GetValue();
+            Console.WriteLine($"Received: {received}");
+        }
+    }
+}
