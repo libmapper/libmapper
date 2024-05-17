30f7556c4ce50b048c04d9df945edb2e3528955a
Kyle Smith <kyleafmine@gmail.com>
2024-05-17 11:42:01 -0300

    CSharp: Add standard documentation comments to Map.cs

--- bindings/csharp/Mapper.NET/Map.cs
+++ bindings/csharp/Mapper.NET/Map.cs
@@ -2,6 +2,10 @@ using System.Runtime.InteropServices;
 
 namespace Mapper.NET;
 
+/// <summary>
+/// Maps define dataflow connections between sets of signals.
+/// A map consists of one or more sources, one destination, and properties which determine how the source data is processed.
+/// </summary>
 public class Map : MapperObject
     {
         public enum Location
@@ -30,18 +34,30 @@ public class Map : MapperObject
 
         [DllImport("mapper", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         private static extern unsafe IntPtr mpr_map_new(int num_srcs, void* srcs, int num_dsts, void* dsts);
-        public unsafe Map(Signal source, Signal destination)
+        
+        /// <summary>
+        /// Create a direct map from one signal to another.
+        /// </summary>
+        /// <param name="source">Signal producing a value</param>
+        /// <param name="destination">Signal consuming the value</param>
+        public Map(Signal source, Signal destination)
         {
-            fixed (void* s = &source._obj)
+            unsafe
             {
-                fixed (void* d = &destination._obj)
+                fixed (void* s = &source._obj)
                 {
-                    _obj = mpr_map_new(1, s, 1, d);
+                    fixed (void* d = &destination._obj)
+                    {
+                        _obj = mpr_map_new(1, s, 1, d);
+                    }
                 }
             }
         }
 
-        // construct from mpr_map pointer
+        /// <summary>
+        /// Create a wrapper around an already existing map.
+        /// </summary>
+        /// <param name="map">Pointer to already existing map</param>
         internal Map(IntPtr map) : base(map) {}
 
         [DllImport("mapper", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
@@ -50,6 +66,18 @@ public class Map : MapperObject
                                                           IntPtr sig3, IntPtr sig4, IntPtr sig5,
                                                           IntPtr sig6, IntPtr sig7, IntPtr sig8,
                                                           IntPtr sig9, IntPtr sig10);
+        /// <summary>
+        /// Creates a map from a string expression. `%y` is used as the target signal and %x is used for a source.
+        /// </summary>
+        /// <example>
+        /// <code>
+        ///     var map = new Map("%y=%x", signal1, signal2); // identity map
+        /// 
+        ///     var map2 = new Map("%y=%x*2.0", signal1, signal2); // double the input
+        /// </code>
+        /// </example>
+        /// <param name="expression"></param>
+        /// <param name="signals"></param>
         public Map(string expression, params Signal[] signals)
         {
             var a = new IntPtr[10];
@@ -84,6 +112,10 @@ public class Map : MapperObject
             return this;
         }
 
+        /// <summary>
+        /// Pushes this map out to the distributed graph, allowing it to become active
+        /// </summary>
+        /// <returns>The same map to allow for chaining</returns>
         public new Map Push()
         {
             base.Push();
@@ -92,6 +124,11 @@ public class Map : MapperObject
 
         [DllImport("mapper", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         private static extern void mpr_map_refresh(IntPtr map);
+        
+        /// <summary>
+        /// Re-create stale map if necessary.
+        /// </summary>
+        /// <returns>The same instance to allow for chaining</returns>
         public Map Refresh()
         {
             mpr_map_refresh(_obj);
@@ -100,6 +137,10 @@ public class Map : MapperObject
 
         [DllImport("mapper", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         private static extern void mpr_map_release(IntPtr map);
+        
+        /// <summary>
+        /// Release this map, removing it from the distributed graph.
+        /// </summary>
         public void Release()
         {
             mpr_map_release(_obj);
@@ -108,6 +149,12 @@ public class Map : MapperObject
 
         [DllImport("mapper", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         private static extern IntPtr mpr_map_get_sigs(IntPtr map, int loc);
+        
+        /// <summary>
+        /// Get a list of signals connected with this map.
+        /// </summary>
+        /// <param name="location">Filter the returned list by signal location</param>
+        /// <returns>A possibly filtered list of signals connected by this map</returns>
         public MapperList<Signal> GetSignals(Location location = Location.Any)
         {
             return new MapperList<Signal>(mpr_map_get_sigs(_obj, (int)location), Type.Signal);
@@ -115,6 +162,12 @@ public class Map : MapperObject
 
         [DllImport("mapper", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.StdCall)]
         private static extern int mpr_map_get_sig_idx(IntPtr map, IntPtr sig);
+        
+        /// <summary>
+        /// Retrieve the index for a specific map signal.
+        /// </summary>
+        /// <param name="signal">The signal to get the numerical index of</param>
+        /// <returns>Numerical signal index</returns>
         public int GetSignalIndex(Signal signal)
         {
             return mpr_map_get_sig_idx(_obj, signal._obj);
